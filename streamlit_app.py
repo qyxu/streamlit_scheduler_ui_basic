06939588242ec955ef430fa4255d64f05dcb21d1
Qiuyan Xu <46363760+qyxu@users.noreply.github.com>
2025-04-23 22:53:25 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,7 +14,7 @@ col1, col2 = st.columns([1, 6])
 with col1:
     st.image(logo, width=100)
 with col2:
-    st.markdown("## Unified Job Shop Scheduler")
+    st.markdown("## Job Scheduler Demo")
 
 st.markdown("---")
 
